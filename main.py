c386d5e1cdb74a50a1be369aa2744b60524d0dd5
nguyenminh <minh.nguyen20@metropolia.fi>
2022-10-11 15:15:36 +0300

    test01

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 
 from tabulate import tabulate
 import mysql.connector
@@ -13,21 +13,7 @@ testDB = mysql.connector.connect(
 )
 from geopy import distance
 mycursor = testDB.cursor()
-"""
-
-
-
-areaCode = input("Input the area code (i.e. FI): ")
-mycursor = testDB.cursor()
-mycursor.execute(f"select country.name as 'country name', airport.name as 'airport name', airport.type as 'airport type', airport.ident from country, airport where country.iso_country = airport.iso_country and airport.iso_country = '{areaCode}' and airport.type = 'm_airport'  order by airport.type;")
-result = mycursor.fetchall()
-print(tabulate(result, tablefmt="fancy_grid"))
-print(mycursor.rowcount, 'rows in set')
-"""
-
-
 #
-CO2_consume = 18
 def Cal(icao_1,icao_2):
     mycursor.execute(f"select name from airport where ident = '{icao_1}';")
     reTurn = mycursor.fetchall()
@@ -39,18 +25,40 @@ def Cal(icao_1,icao_2):
     listDeg = []
     for x in mycursor:
         listDeg.append(x)
-    return distance.distance(listDeg[0], listDeg[1])
+    return distance.distance(listDeg[0], listDeg[1]).km
+rand = random.randint(2,4)
+rand1 = rand
+rand2 = rand
+print(f"You have to transit {rand-1} point")
 A = "EFHK"
-B = input("Choose next airport: ")
-while B != "ivalo":
-    xx = Cal(A,B)
-    CO2_consume = CO2_consume - (xx.km * 0.1)
-    if CO2_consume > 0:
-        print(f"{xx.km:.2f}")
-        print(f"You released {xx.km*0.1:.2f}kg CO2 after the flight")
-        print(f"The remaining {CO2_consume:.2f}kg of CO2 is allowed to consume")
+listc1 = []
+listc2 = []
+if rand1 > 1:
+    A = "EFHK"
+    for x in range(1, rand1):
+        B = input("Player_1:Choose next airport: ")
+        listc1.append(Cal(A,B))
         A = B
-    elif CO2_consume < 0:
-        print(f"You need {CO2_consume * -1:.2f}kg CO2 to go {B}")
-    B = input("Choose next airport: ")
+else:
+    listc1.append(Cal(B,"EFIV"))
+    mycursor.execute(f"select name from airport where ident = EFIV;")
+    reTurn = mycursor.fetchall()
+    print(tabulate(reTurn, tablefmt="fancy_grid"))
+if rand2 > 1:
+    A = "EFHK"
+    for x in range(1, rand2):
+        B = input("Player_2:Choose next airport: ")
+        listc2.append(Cal(A,B))
+        A = B
+else:
+        listc2.append(Cal(B,"EFIV"))
+        mycursor.execute(f"select name from airport where ident = EFIV;")
+        reTurn = mycursor.fetchall()
+        print(tabulate(reTurn, tablefmt="fancy_grid"))
+print(f"Player_1 went a total of {sum(listc1):.2f}km to find Snorkmaiden")
+print(f"Player_2 went a total of {sum(listc2):.2f}km to find Snorkmaiden")
+if sum(listc1) > sum(listc2):
+    print("Layer_1 is Moomin\nLayer_2 is the loser" )
+elif sum(listc1) < sum(listc2):
+    print("Layer_1 is the loser\nLayer_2 is Moomin" )
 #
